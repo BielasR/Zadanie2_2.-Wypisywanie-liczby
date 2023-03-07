0141ec41f738a75b506698d5b2f0a8f7edcb38c1
BielasR <126687810+BielasR@users.noreply.github.com>
2023-03-07 20:46:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    Scanner l = new Scanner(System.in);
+    System.out.println("Podaj liczbę");
+    int liczba = l.nextInt();
+    System.out.println("Podana Liczba to " + liczba);
+  }
+}
